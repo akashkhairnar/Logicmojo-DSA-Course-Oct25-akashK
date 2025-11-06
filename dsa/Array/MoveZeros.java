7758505bc220574e2a5471c6ccd4e800e3afaf2c
akashkhairnar <akashkhairnar40@gmail.com>
2025-11-06 08:47:36 +0530

    two sum

--- dsa/Array/MoveZeros.java
+++ dsa/Array/MoveZeros.java
@@ -1,7 +1,7 @@
 // Problem:  Move Zeroes
 // Link: https://leetcode.com/problems/move-zeroes/description.
 // Notes: Count zeros and if count>0 then swap no by that much index
-// Level : Easy
+// Level: Easy
 // Time Complexity: O(n)
 
 public void moveZeroes(int[] nums) {
