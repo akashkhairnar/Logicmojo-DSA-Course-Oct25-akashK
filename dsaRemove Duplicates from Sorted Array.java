3edda5b84d15decb983d139d663c6434626841b7
Akash Khairnar <35635008+akashkhairnar@users.noreply.github.com>
2025-11-15 09:22:00 +0530

    Create dsaRemove Duplicates from Sorted Array.java

---
+++ dsaRemove Duplicates from Sorted Array.java
@@ -0,0 +1,29 @@
+// Problem:  26. Remove Duplicates from Sorted Array
+// Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+// Notes:  two pointer if find different element then update element of slow 
+// Level: Easy
+// Pattern: Two Pointer
+// Revisit: no
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        int j=i+1;
+        int n= nums.length;
+
+        while(j<n){
+            if(nums[i]==nums[j]){
+                j++;
+            }
+            else if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+                j++;
+            }
+             
+        }
+        return i+1;
+        
+    }
+}
