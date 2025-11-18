b6a37dc9171f068e2e7438ef610c71f8eed7972f
Akash Khairnar <35635008+akashkhairnar@users.noreply.github.com>
2025-11-18 21:39:36 +0530

    Create reverse-linked-list.java

---
+++ dsa/LinkedList/reverse-linked-list.java
@@ -0,0 +1,37 @@
+// Problem:  206. Reverse Linked List
+// Link: https://leetcode.com/problems/reverse-linked-list/
+// Notes:  two point prev & curr swap one link at a time 
+// Level: Easy
+// Pattern: Two Pointer
+// Revisit: no
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+
+    }
+
+}
